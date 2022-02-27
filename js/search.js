334b83b2f4c4faa8109f4b06360074bf06244851
Hanna Ngo <98720596+hannango@users.noreply.github.com>
2022-02-26 23:50:43 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -2,6 +2,6 @@ function openPage() {
     var x = document.getElementById("search").value; 
 
     if(x === "11211") { 
-        window.location.replace("Zipcodes/11211.html");
+        window.location.replace("./Zipcodes/11211.html");
     }
 }
